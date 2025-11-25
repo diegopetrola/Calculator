a0dabefd7a6ced91c9b25b7033d07f2b7b31020d
diegopetrola <diegopetrola@gmail.com>
2025-11-25 18:54:33 -0300

    Refactor and implemented history

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,116 +1,129 @@
-﻿using System.Text.RegularExpressions;
+﻿using CalculatorLibrary;
+using System.Text.RegularExpressions;
 
-class Program
+namespace CalculatorProgram
 {
-    static void Main(string[] args)
+    class Program
     {
-        bool endApp = false;
-        // Display title as the C# console calculator app.
-        Console.WriteLine("Console Calculator in C#\r");
-        Console.WriteLine("------------------------\n");
-
-        while (!endApp)
+        public Calculator calculator;
+        Program()
         {
-            // Declare variables and set to empty.
-            // Use Nullable types (with ?) to match type of System.Console.ReadLine
-            string? numInput1 = "";
-            string? numInput2 = "";
-            double result = 0;
+            calculator = new();
+        }
 
-            // Ask the user to type the first number.
-            Console.Write("Type a number, and then press Enter: ");
-            numInput1 = Console.ReadLine();
-
-            double cleanNum1 = 0;
-            while (!double.TryParse(numInput1, out cleanNum1))
+        void printHistory()
+        {
+            var history = calculator.GetHistory();
+            for (int i = 0; i < history.Count; i++)
             {
-                Console.Write("This is not valid input. Please enter a numeric value: ");
-                numInput1 = Console.ReadLine();
+                Operation op = history.ElementAt(i);
+                Console.WriteLine($"[{i}]- {op.Num1} {op.Operand} {op.Num2} = {op.result}");
             }
 
-            // Ask the user to type the second number.
-            Console.Write("Type another number, and then press Enter: ");
-            numInput2 = Console.ReadLine();
+            Console.WriteLine("Obs: type uN (where N is a integer) to use the results above in your operations");
+            Console.WriteLine("\n");
+        }
 
-            double cleanNum2 = 0;
-            while (!double.TryParse(numInput2, out cleanNum2))
+        bool ParseInput(out double value)
+        {
+            value = 0;
+            string input = Console.ReadLine() ?? "";
+            if (input.Length == 0) return false;
+            if (input[0] == 'h')
             {
-                Console.Write("This is not valid input. Please enter a numeric value: ");
-                numInput2 = Console.ReadLine();
+                printHistory();
+                return false;
             }
-
-            // Ask the user to choose an operator.
-            Console.WriteLine("Choose an operator from the following list:");
-            Console.WriteLine("\ta - Add");
-            Console.WriteLine("\ts - Subtract");
-            Console.WriteLine("\tm - Multiply");
-            Console.WriteLine("\td - Divide");
-            Console.Write("Your option? ");
-
-            string? op = Console.ReadLine();
-
-            // Validate input is not null, and matches the pattern
-            if (op == null || !Regex.IsMatch(op, "[a|s|m|d]"))
+            else if (input[0] == 'u')
             {
-                Console.WriteLine("Error: Unrecognized input.");
-            }
-            else
-            {
-                try
+                if (int.TryParse(input.Substring(1, 1), out int index))
                 {
-                    result = Calculator.DoOperation(cleanNum1, cleanNum2, op);
-                    if (double.IsNaN(result))
+                    var history = calculator.GetHistory();
+                    if (index < history.Count)
                     {
-                        Console.WriteLine("This operation will result in a mathematical error.\n");
+                        value = calculator.GetHistory().ElementAt(index).result;
+                        Console.WriteLine($"...used value {value} from history.");
+                        return true;
                     }
-                    else Console.WriteLine("Your result: {0:0.##}\n", result);
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine("Oh no! An exception occurred trying to do the math.\n - Details: " + e.Message);
+                    else return false;
                 }
             }
+            else if (double.TryParse(input, out value))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        static public void Main(string[] args)
+        {
+            Program app = new();
+
+            bool endApp = false;
+            Console.WriteLine("Console Calculator in C#\r");
+            Console.WriteLine($"Calculator has been used {app.calculator.getOpenedCount()} times");
             Console.WriteLine("------------------------\n");
 
-            // Wait for the user to respond before closing.
-            Console.Write("Press 'n' and Enter to close the app, or press any other key and Enter to continue: ");
-            if (Console.ReadLine() == "n") endApp = true;
+            while (!endApp)
+            {
+                double result = 0;
 
-            Console.WriteLine("\n"); // Friendly linespacing.
-        }
-        return;
-    }
-}
-
-class Calculator
-{
-    public static double DoOperation(double num1, double num2, string op)
-    {
-        double result = double.NaN; // Default value is "not-a-number" if an operation, such as division, could result in an error.
-
-        // Use a switch statement to do the math.
-        switch (op)
-        {
-            case "a":
-                result = num1 + num2;
-                break;
-            case "s":
-                result = num1 - num2;
-                break;
-            case "m":
-                result = num1 * num2;
-                break;
-            case "d":
-                // Ask the user to enter a non-zero divisor.
-                if (num2 != 0)
+                // Ask the user to type the first number.
+                Console.Write("Type a number, or type 'h' to see history.");
+                double cleanNum1 = 0;
+                while (!app.ParseInput(out cleanNum1))
                 {
-                    result = num1 / num2;
+                    Console.Write("This is not valid input. Please enter an integer value: ");
                 }
-                break;
-            // Return text for an incorrect option entry.
-            default:
-                break;
+
+                // Ask the user to type the second number.
+                Console.Write("Type another number, and then press Enter: ");
+                double cleanNum2 = 0;
+                while (!app.ParseInput(out cleanNum2))
+                {
+                    Console.Write("This is not valid input. Please enter an integer value: ");
+                }
+
+                // Ask the user to choose an operator.
+                Console.WriteLine("Choose an operator from the following list:");
+                Console.WriteLine("\ta - Add");
+                Console.WriteLine("\ts - Subtract");
+                Console.WriteLine("\tm - Multiply");
+                Console.WriteLine("\td - Divide");
+                Console.Write("Your option? ");
+
+                string? op = Console.ReadLine();
+
+                // Validate input is not null, and matches the pattern
+                if (op == null || !Regex.IsMatch(op, "[a|s|m|d]"))
+                {
+                    Console.WriteLine("Error: Unrecognized input.");
+                }
+                else
+                {
+                    try
+                    {
+                        result = app.calculator.DoOperation(cleanNum1, cleanNum2, op);
+                        if (double.IsNaN(result))
+                        {
+                            Console.WriteLine("This operation will result in a mathematical error.\n");
+                        }
+                        else Console.WriteLine("Your result: {0:0.##}\n", result);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("Oh no! An exception occurred trying to do the math.\n - Details: " + e.Message);
+                    }
+                }
+                Console.WriteLine("------------------------\n");
+
+                // Wait for the user to respond before closing.
+                Console.Write("Press 'n' and Enter to close the app, or press any other key and Enter to continue: ");
+                if (Console.ReadLine() == "n") endApp = true;
+
+                Console.WriteLine("\n"); // Friendly linespacing.
+            }
+            return;
         }
-        return result;
     }
 }
---
+++ CalculatorLibrary/CalculatorLibrary.cs
@@ -0,0 +1,124 @@
+﻿// OK! Create a functionality that will count the amount of times the calculator was used.
+// Ok! Store a list with the latest calculations. And give the users the ability to delete that list.
+// OK! Allow the users to use the results in the list above to perform new calculations.
+// OK! Add extra calculations: Square Root, Taking the Power, 10x, Trigonometry functions.
+
+using System.Text.Json;
+using System.IO;
+
+namespace CalculatorLibrary
+{
+    public class Calculator
+    {
+        const string logPath = "../../../calculatorlog.json";
+        private CalculatorLog log;
+
+        public Calculator()
+        {
+            string jsonString = File.Exists(logPath) ? File.ReadAllText(logPath) : "{}";
+            log = JsonSerializer.Deserialize<CalculatorLog>(jsonString) ?? new CalculatorLog();
+            log.openedCount++;
+            WriteLog();
+        }
+
+        public int getOpenedCount()
+        {
+            return log.openedCount;
+        }
+
+        public List<Operation> GetHistory()
+        {
+            return log.history;
+        }
+
+        void WriteLog()
+        {
+            int len = log.history.Count;
+            // Only write the last 5 elements, both to avoid cluttering the screen and logs too large
+            if (len > 5)
+                log.history = log.history.GetRange(len - 6, len - 1);
+            Console.WriteLine(JsonSerializer.Serialize(log));
+            Console.WriteLine(log.openedCount);
+            File.WriteAllText(logPath, JsonSerializer.Serialize<CalculatorLog>(log));
+        }
+
+        public double DoOperation(double num1, double num2, string op)
+        {
+            Operation operand = new();
+            operand.result = double.NaN;
+            operand.Num1 = num1;
+            operand.Num2 = num2;
+            log.history.Add(operand);
+
+            // Use a switch statement to do the math.
+            switch (op)
+            {
+                case "a":
+                    operand.result = num1 + num2;
+                    operand.Operand = "Add";
+                    break;
+                case "s":
+                    operand.result = num1 - num2;
+                    operand.Operand = "Subtract";
+                    break;
+                case "m":
+                    operand.result = num1 * num2;
+                    operand.Operand = "Multiply";
+                    break;
+                case "d":
+                    // Ask the user to enter a non-zero divisor.
+                    if (num2 != 0)
+                    {
+                        operand.result = num1 / num2;
+                        operand.Operand = "Divide";
+                    }
+                    else
+                    {
+                        log.history.RemoveAt(log.history.Count - 1); // remove from history
+                    }
+                    break;
+                case "p":
+                    operand.result = Math.Pow(num1, num2);
+                    operand.Operand = "Power";
+                    break;
+                case "q":
+                    if (num1 > 0)
+                    {
+                        operand.result = Math.Sqrt(num1);
+                        operand.Operand = "Square Root";
+                    }
+                    else
+                    {
+                        log.history.RemoveAt(log.history.Count - 1);
+                    }
+                    break;
+                case "c":
+                    operand.result = Math.Cos(num1);
+                    operand.Operand = "Consine";
+                    break;
+                case "i":
+                    operand.result = Math.Sin(num1);
+                    operand.Operand = "Consine";
+                    break;
+                // Return text for an incorrect option entry.
+                default:
+                    break;
+            }
+            WriteLog();
+
+            return operand.result;
+        }
+    }
+    public class Operation
+    {
+        public double Num1 { get; set; }
+        public double Num2 { get; set; }
+        public string? Operand { get; set; }
+        public double result { get; set; }
+    }
+    public class CalculatorLog
+    {
+        public int openedCount { get; set; }
+        public List<Operation> history { get; set; } = [];
+    }
+}
